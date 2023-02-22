78c3941893f5a73c7ab8466a4d7a35fc228ebbba
Yozhylo <polidar2012@gmail.com>
2023-02-22 12:46:34 +0200

    minecraft-server: fixed init script
    Finally made file an executable on linux, and set 'sh' as executable
    instead of 'bash'.

--- minecraft-gtnh/init.sh
+++ minecraft-gtnh/init.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 echo "Initialising docker"
 docker build -tgregtech-new-horizons . && docker run -d -p25565:25565/tcp -p27015:27015/tcp --name=gtnh-server --restart=unless-stopped gregtech-new-horizons
