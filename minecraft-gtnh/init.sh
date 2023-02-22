6c5ddcf219142ba384adbe0efc74f53a671e1675
Yozhylo <polidar2012@gmail.com>
2023-02-22 12:16:28 +0200

    minecraft-server: added port publish and restart policy to init script

--- minecraft-gtnh/init.sh
+++ minecraft-gtnh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Initialising docker"
-docker build -tgregtech-new-horizons . && docker run -d --name gtnh-server gregtech-new-horizons
+docker build -tgregtech-new-horizons . && docker run -d -p25565:25565/tcp -p27015:27015/tcp --name=gtnh-server --restart=unless-stopped gregtech-new-horizons
